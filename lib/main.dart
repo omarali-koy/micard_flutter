77324c09899edf2c28ccfcf605edbef9c3ee4495
omarali-koy <omarali.linux@gmail.com>
2021-05-07 19:42:56 +0600

    Added fonts and avatar profile

--- lib/main.dart
+++ lib/main.dart
@@ -6,46 +6,23 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.red,
-                child: Text('Container 1'),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/palestra_block.png'),
+            ),
+            Text(
+              'Palestra blocks',
+              style: TextStyle(
+                fontFamily: 'ArchitectsDaughter',
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Container(
-                width: 100.0,
-                color: Colors.lightBlue,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.yellow,
-                      child: Text('Container 1'),
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.green,
-                      child: Text('Container 1'),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.lightBlue,
-                child: Text('Container 2'),
-              ),
-            ],
-          ),
-        ),
+            )
+          ],
+        )),
       ),
     ),
   );
